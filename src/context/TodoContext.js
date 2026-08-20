1928eb45fad0238ff60e261a0d8a69c8a2e84d76
Atta ur Rahman <aur2006.cs@gmail.com>
2026-08-20 15:10:11 +0500

    Fix comment formatting in TodoContext.js
    Updated comment formatting in TodoContext.js.

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-// context
+// context.
 export const TodoContext = createContext({
   // todos array and inside array every object is an indiviual todo/task and we just add one todo cause to remember the format
 
